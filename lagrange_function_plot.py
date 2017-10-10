50789974de15f8200df92f6205cdf3f9ea57ba14
Brendan_Henry <brendanhenry@email.com>
2017-10-10 22:46:22 +0800

    format code

--- lagrange_function_plot.py
+++ lagrange_function_plot.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import lagrange
 
-x = np.linspace(-5,5,1000)
+x = np.linspace(-5, 5, 1000)
 ax = plt.subplot()
 
-ax.plot(x,1/(1+x**2),label="f(x)")
-for n in [2,4,8,10]:
-    x_n=[-5+(10/n)*i for i in range (n+1)]
-    y_n=[1/(1+x**2) for x in x_n]
-    lag=lagrange(x_n,y_n)
-    ax.plot(x,lag(x),label="n={0}".format(n))
+ax.plot(x, 1 / (1 + x**2), label="f(x)")
+for n in [2, 4, 8, 10]:
+    x_n = [-5 + (10 / n) * i for i in range(n + 1)]
+    y_n = [1 / (1 + x**2) for x in x_n]
+    lag = lagrange(x_n, y_n)
+    ax.plot(x, lag(x), label="n={0}".format(n))
 ax.grid()
 ax.legend()
 plt.show()
