c37865962284e84eef4883e6acfebf5456de82f7
Brendan_Henry <brendanhenry@email.com>
2017-10-10 22:42:14 +0800

    refactor code

--- lagrange_function_plot.py
+++ lagrange_function_plot.py
@@ -1,45 +1,17 @@
 #%%
-import matplotlib.pyplot as plt 
-import numpy as np 
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.interpolate import lagrange
 
-x = np.arange(-5,5,0.001)
-x1 = [-5,0,5]
-x2 = [-5,-2.5,0,2.5,5]
-x3 = [-5,-3.75,-2.5,-1.25,0,1.25,2.5,3.75,5]
-x4 = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
-y1=y2=y3=y4=[]
-for a in x1 :
-    y1.append(1/(1+a*a))
-p1 = lagrange(x1,y1)
-for a in x2 :
-    y2.append(1/(1+a*a))
-p2 = lagrange(x2,y2)
-for a in x3 :
-    y3.append(1/(1+a*a))
-p3 = lagrange(x3,y3)
-for a in x4 :
-    y4.append(1/(1+a*a))
-p4 = lagrange(x4,y4)
+x = np.linspace(-5,5,1000)
+ax = plt.subplot()
 
-
-
-fig, ax = plt.subplots()
-ax.plot(x, p1(x))
-ax.plot(x, p2(x))
-ax.plot(x, p3(x))
-ax.plot(x, p4(x))
-ax.plot(x,1/(1+x*x))
+ax.plot(x,1/(1+x**2),label="f(x)")
+for n in [2,4,8,10]:
+    x_n=[-5+(10/n)*i for i in range (n+1)]
+    y_n=[1/(1+x**2) for x in x_n]
+    lag=lagrange(x_n,y_n)
+    ax.plot(x,lag(x),label="n={0}".format(n))
 ax.grid()
-<<<<<<< HEAD
-
-=======
->>>>>>> ba172d051110c0e4bd3c7e835f6cba78e07ff3ce
-plt.show()
-
-fig2,ax2=plt.subplots()
-ax2.plot(x,1/(1+x*x))
-ax2.grid()
-
-
+ax.legend()
 plt.show()
