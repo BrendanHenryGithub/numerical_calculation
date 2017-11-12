0b75ff76363a5eb6024a7cd45f1f2eb4b01de072
Brendan <email@email.com>
2017-11-12 15:37:41 +0800

    add

--- cubic_spline_interpolation.py
+++ cubic_spline_interpolation.py
@@ -5,6 +5,8 @@ from scipy.interpolate import lagrange
 
 def f(x):
     return 1 / (1 + x**2)
+    # y_list=[0,0.79,1.53,2.19,2.71,3.03,3.27,2.89,3.06,3.19,3.29]   #车门曲线绘图
+    # return y_list[x]
 
 def first_difference(x_1, x_2):
     return (f(x_1) - f(x_2)) / (x_1 - x_2)
@@ -67,7 +69,7 @@ def cubic_spline_interpolation(x_list, fx_list, left_derivative, right_derivativ
                                                                                                                                 * h_list[j]**2 / 6) * (x_list[j + 1] - x) / h_list[j] + (fx_list[j + 1] - M_list[j + 1] * h_list[j]**2 / 6) * (x - x_list[j]) / h_list[j], label="{0}< x <{1}".format(x_list[j],x_list[j+1]))
     ax.grid()
     ax.legend()
-    plt.savefig("tmp_cubic.png")
+    plt.savefig("tmp_cubic1.png")
     plt.show()
 
     return 0
@@ -79,20 +81,25 @@ def main():
     left_derivative = 10 / 26**2
     right_derivative = -10 / 26**2
 
-    x = np.linspace(-5, 5, 1000)      
-    ax = plt.subplot()
+    # x = np.linspace(-5, 5, 1000)      
+    # ax = plt.subplot()
 
-    ax.plot(x, 1 / (1 + x**2), label="f(x)")  #f(x)绘图
-    for n in [10]:
-        x_n = [-5 + (10 / n) * i for i in range(n + 1)]
-        y_n = [1 / (1 + x**2) for x in x_n]
-        lag = lagrange(x_n, y_n)
-        ax.plot(x, lag(x), label="{0}".format("lag-10"))  #10次拉格让日绘图
-    ax.grid()
-    ax.legend()
-    plt.savefig("tmp_cubic2.png")    
-    plt.show()
+    # ax.plot(x, 1 / (1 + x**2), label="f(x)")  #f(x)绘图
+    # for n in [10]:
+    #     x_n = [-5 + (10 / n) * i for i in range(n + 1)]
+    #     y_n = [1 / (1 + x**2) for x in x_n]
+    #     lag = lagrange(x_n, y_n)
+    #     ax.plot(x, lag(x), label="{0}".format("lag-10"))  #10次拉格让日绘图
+    # ax.grid()
+    # ax.legend()
+    # plt.savefig("tmp_cubic2.png")    
+    # plt.show()
     
+    # x_list = [i for i in range(11)]
+    # fx_list = [f(x) for x in x_list]
+    # left_derivative=0.8
+    # right_derivative=0.2
+
     #三次样条插值绘图
     cubic_spline_interpolation(
         x_list, fx_list, left_derivative, right_derivative)
